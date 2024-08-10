aaf6079cf136690478a084af74e73fb2db325976
Daeyan_server <daeyansidi826@gmail.com>
2024-08-10 19:12:21 +0500

    header and main done

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleMenu() {
+    const menu = document.getElementById('menuDropDown');
+    menu.classList.remove('hidden');
+}
+
+function handleMenuClose() {
+    const menu = document.getElementById('menuDropDown');
+    menu.classList.add('hidden');
+}
